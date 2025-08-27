31de7c392ddb83f6c487d31d756b43365c8bc75d
Sneha Mungre <snehamungre@gmail.com>
2025-08-27 18:44:52 +0400

    scroll added additional items

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -13,15 +13,27 @@ const Entry = ({ onComplete }) => {
             { opacity: 1, y: 0, duration: 1, delay: 2, ease:'power.in'}
         );
 
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: "#intro-boxes",
+                start: "top top",   // when section is 80% down viewport
+                end: "+=150",     // finish earlier
+                scrub: true,
+                markers: false
+            }
+        })
+            .from("#left-box", { x: "-100vw", opacity: 0, duration: 1, ease: "power3.out" })
+            .from("#right-box", { x: "100vw", opacity: 0, duration: 1, ease: "power3.out" }, "<");
+
         // Video animation timeline
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: '#video',
                 start: "bottom bottom",
-                end: "center center",
+                end: "+=1100",
                 scrub: true,
                 pin: true,
-                markers:false,
+                markers:true,
                 onLeave: () => {
                     // when scroll animation is finished
                     if (onComplete) onComplete();
@@ -37,12 +49,29 @@ const Entry = ({ onComplete }) => {
     }, [onComplete]);
 
     return (
-        <div className="flex-column  w-full">
+        <div className="flex-column w-full relative">
             {/* Instruction Text */}    
-            <div className="h-dvh w-full">
+            <div className="h-dvh w-full relative">
+                <div id="intro-boxes" className="relative h-screen pointer-events-none z-10">
+                    {/* Left box - positioned at top of viewport */}
+                    <img
+                        id="left-box"
+                        src={'assets/images/left-box.png'}
+                        alt={'left-box'}
+                        className="absolute top-50 h-90 -translate-x-14"
+                    />
+                    {/* Right box - positioned at bottom of viewport */}
+                    <img id="right-box"
+                         src={'assets/images/right-box.png'}
+                         alt={'right-box'}
+                         className="absolute bottom-10 right-0 w-60 rotate-20 translate-x-24"
+                    />
+                </div>
+
+
                 <p
                     id="scroll-instruction"
-                    className="absolute bottom-0 w-full transform y-full text-lg text-green-100 mb-6 font-large text-center tracking-wide font-bold animate-bounce"
+                    className="absolute bottom-3 left-0 right-0 text-lg text-green-100 mb-6 font-large text-center tracking-wide font-bold animate-bounce"
                 >
                     Scroll slowly ↓
                 </p>
